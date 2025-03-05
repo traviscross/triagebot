181044a5e6d7dd05af386b3f9106840a1004c769
Travis Cross <tc@traviscross.com>
2025-03-05 15:34:59 +0000

    Bump lang to Rust 2025 edition

--- src/agenda.rs
+++ src/agenda.rs
@@ -573,7 +573,7 @@ pub fn lang<'a>() -> Box<dyn Action + Send + Sync> {
                     kind: QueryKind::List,
                     query: Arc::new(github::Query {
                         filters: vec![("state", "open")],
-                        include_labels: vec!["A-edition-2024", "C-tracking-issue", "T-lang"],
+                        include_labels: vec!["A-edition-2025", "C-tracking-issue", "T-lang"],
                         exclude_labels: vec!["S-tracking-ready-for-edition"],
                     }),
                 }],
